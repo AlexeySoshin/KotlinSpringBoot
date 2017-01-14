0c0e79c1483d620570d889c70be26399d5f0315a
Alexey Soshin <alexey.soshin@gettaxi.com>
2017-01-14 08:30:12 +0200

    Initial commit

---
+++ src/main/kotlin/com/soshin/Controller.kt
@@ -0,0 +1,24 @@
+package com.soshin
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+
+/**
+ * This controller should return JSON response containing current timestamp
+ */
+/*
+@RestController
+class Controller {
+
+    @RequestMapping("/")
+    @ResponseBody // Spring will handle the response
+    internal fun home(): homeResponse {
+
+        return homeResponse(System.currentTimeMillis())
+    }
+}
+
+// No need for setters getters for a simple POJO
+data class homeResponse (val time : Long, val message : String? = null)*/
